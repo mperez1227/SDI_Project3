1dd236e492371a839d1929be3a115b235c1e0c27
Miriam Perez <mperez1227@fullsail.edu>
2012-01-16 23:36:23 -0500

    Added Global Variables.

--- Perez_Miriam_assignment3.js
+++ Perez_Miriam_assignment3.js
@@ -1,1 +1,23 @@
-alert("JavaScript works!");
+/* Miriam Perez
+January 16, 2012
+Deliverable 3
+"Fun Times"
+*/
+
+//alert("JavaScript works!");
+
+//Global Variables
+
+var movieType1 = "comedy",
+	movieType2 = "action",
+	movieType3 = "drama",
+	actionFilm = '"Mission: Impossible"',
+	comedyFilm = '"Jack & Jill"',
+	dramaFilm = "Girl with the dragon tattoo",
+	destination1 = "movies",
+	destination2 = "dinner",
+	destination3 = "play pool",
+	thisWeekend = "Saturday",
+	numberOfPeople = 4,
+	thisSaturday = true,
+	thisFriday = false;
